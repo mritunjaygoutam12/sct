5e14fcf1841f935cddbbdeb159f9f167d483de17
mritunjaygoutam12 <mritunjaygoutam2204@gmail.com>
2019-04-13 03:45:33 +0530

    chart dep added

--- src/views/stats.vue
+++ src/views/stats.vue
@@ -4,6 +4,7 @@
     <chart>
     <h3>Stats of his century throughtout his carrier.</h3>
     <p>dfbdsfb</p>
+    <area-chart :data=data></area-chart>
     </chart>
   </div>
 </template>
@@ -11,6 +12,11 @@
 <script>
 // @ is an alias to /src
 import chart from '@/components/chart.vue'
+import Vue from 'vue'
+import VueChartkick from 'vue-chartkick'
+import Chart from 'chart.js'
+
+Vue.use(VueChartkick, {adapter: Chart})
 
 export default {
   name: 'home',
